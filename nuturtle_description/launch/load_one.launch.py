4c95aa08fd609d2be1e6b44e7d54f2f75164213a
Ayush Gaggar <gaggarayush@gmail.com>
2023-01-09 14:42:35 -0600

    finished task a3

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -34,6 +34,7 @@ def generate_launch_description():
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
+            # namespace='',
             condition=LaunchConfigurationEquals('use_jsp', 'true')
         ),
         Node(
