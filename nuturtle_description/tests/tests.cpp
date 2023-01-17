2b75d7efd09f6054b3d5ebf13b03f594248658a4
Ayush Gaggar <gaggarayush@gmail.com>
2023-01-17 13:37:08 -0600

    first test case

--- nuturtle_description/tests/tests.cpp
+++ nuturtle_description/tests/tests.cpp
@@ -1,2 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
-// #include <features-time64.h>
+#include "turtlelib/rigid2d.hpp"
+
+TEST_CASE("Rotate and Translate", "[transform2d]") {
+    float transx = 1.0;
+    float transy = 1.0;
+    float rad = turtlelib::PI/4;
+    turtlelib::Transform2D transform_test = turtlelib::Transform2D(turtlelib::Vector2D{transx, transy}, rad);
+    REQUIRE( transform_test.translation().x == transx);
+    REQUIRE( transform_test.translation().y == transy);
+    REQUIRE( transform_test.rotation() == rad);
+}
