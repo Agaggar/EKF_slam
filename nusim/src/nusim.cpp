b712d28c949946195021b7d01e061caac8a41cb7
Ayush Gaggar <gaggarayush@gmail.com>
2023-01-19 14:46:50 -0600

    creataed topic

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -2,25 +2,34 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int64.hpp"
 
+using namespace std::chrono_literals;
+
 class Nusim : public rclcpp::Node
 {
     public:
         Nusim():
             Node("nusim"),
-            count_(0)
+            timestep(0)
         {
-            timer = create_wall_timer(std::chrono::duration(5ms), std::bind(&Nusim::timer_callback, this));
-            // rclcpp::create_timer()
+            declare_parameter("rate", 200); // defaults to 200
+            timestep_pub_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
+            timer_ = create_wall_timer(std::chrono::milliseconds(5ms), std::bind(&Nusim::timer_callback, this));
+            // auto ts = std_msgs::msg::UInt64();
         }
 
     private:
         void timer_callback()
         {
-            RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Publish count " << this->count_);
-            count_ += 1;
+            ts.data = timestep;
+            // RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Publish count " << this->count_);
+            // RCLCPP_INFO(get_logger(), "stuff: %ld", timestep);
+            timestep_pub_->publish(ts);
+            timestep += 1;
         }
-        size_t count_;
+        size_t timestep;
+        std_msgs::msg::UInt64 ts;
         rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub_;
 };
 
 int main(int argc, char* argv[])
