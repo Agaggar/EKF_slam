e46fd378cb1ce78d040253d5cd8857286cfe4f9c
Ayush Gaggar <gaggarayush@gmail.com>
2023-01-19 19:49:01 -0600

    working on c.3

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -20,6 +20,28 @@ class Nusim : public rclcpp::Node
             rate_param_desc.description = "simulation refresh rate (hz)";
             declare_parameter("rate", rclcpp::ParameterValue(rate), rate_param_desc); // defaults to 200
             get_parameter("rate", rate);
+            double x0 = 0.0;
+            rcl_interfaces::msg::ParameterDescriptor x0_param_desc;
+            x0_param_desc.name = "x0";
+            x0_param_desc.type = 3; // x0 is a double
+            x0_param_desc.description = "initial x0 position (m)";
+            declare_parameter("x0", rclcpp::ParameterValue(x0), x0_param_desc); // defaults to 0.0
+            get_parameter("x0", x0);
+            double y0 = 0.0;
+                    rcl_interfaces::msg::ParameterDescriptor y0_param_desc;
+            y0_param_desc.name = "y0";
+            y0_param_desc.type = 3; // y0 is a double
+            y0_param_desc.description = "initial y0 position (m)";
+            declare_parameter("y0", rclcpp::ParameterValue(y0), y0_param_desc); // defaults to 0.0
+            get_parameter("y0", y0);
+            double theta = 0.0;
+            rcl_interfaces::msg::ParameterDescriptor theta_param_desc;
+            theta_param_desc.name = "theta";
+            theta_param_desc.type = 3; // theta is a double
+            theta_param_desc.description = "initial theta value (rad)";
+            declare_parameter("theta", rclcpp::ParameterValue(theta), theta_param_desc); // defaults to 0.0
+            get_parameter("theta", theta);
+
             timestep_pub_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
             timer_ = create_wall_timer(std::chrono::milliseconds(int(1.0/rate*1000)), std::bind(&Nusim::timer_callback, this));
             reset_srv_ = create_service<std_srvs::srv::Empty>(
