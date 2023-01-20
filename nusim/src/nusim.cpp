4da9820f2ae9dba8cf8bff5184847d0167a2600b
Ayush Gaggar <gaggarayush@gmail.com>
2023-01-20 00:11:48 -0600

    working on passing in parameters

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -3,6 +3,9 @@
 #include "std_msgs/msg/u_int64.hpp"
 #include "std_srvs/srv/empty.hpp"
 #include "rcl_interfaces/msg/parameter_descriptor.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
 
 using namespace std::chrono_literals;
 
@@ -19,7 +22,7 @@ class Nusim : public rclcpp::Node
             rate_param_desc.type = 3; // rate is a double
             rate_param_desc.description = "simulation refresh rate (hz)";
             declare_parameter("rate", rclcpp::ParameterValue(rate), rate_param_desc); // defaults to 200
-            get_parameter("rate", rate);
+            double rate = get_parameter("rate").get_parameter_value().get<std::double_t>();
             double x0 = 0.0;
             rcl_interfaces::msg::ParameterDescriptor x0_param_desc;
             x0_param_desc.name = "x0";
@@ -28,20 +31,37 @@ class Nusim : public rclcpp::Node
             declare_parameter("x0", rclcpp::ParameterValue(x0), x0_param_desc); // defaults to 0.0
             get_parameter("x0", x0);
             double y0 = 0.0;
-                    rcl_interfaces::msg::ParameterDescriptor y0_param_desc;
+            rcl_interfaces::msg::ParameterDescriptor y0_param_desc;
             y0_param_desc.name = "y0";
             y0_param_desc.type = 3; // y0 is a double
             y0_param_desc.description = "initial y0 position (m)";
             declare_parameter("y0", rclcpp::ParameterValue(y0), y0_param_desc); // defaults to 0.0
             get_parameter("y0", y0);
-            double theta = 0.0;
-            rcl_interfaces::msg::ParameterDescriptor theta_param_desc;
-            theta_param_desc.name = "theta";
-            theta_param_desc.type = 3; // theta is a double
-            theta_param_desc.description = "initial theta value (rad)";
-            declare_parameter("theta", rclcpp::ParameterValue(theta), theta_param_desc); // defaults to 0.0
-            get_parameter("theta", theta);
+            double theta0 = 0.0;
+            rcl_interfaces::msg::ParameterDescriptor theta0_param_desc;
+            theta0_param_desc.name = "theta0";
+            theta0_param_desc.type = 3; // theta0 is a double
+            theta0_param_desc.description = "initial theta value (rad)";
+            declare_parameter("theta0", rclcpp::ParameterValue(theta0), theta0_param_desc); // defaults to 0.0
+            get_parameter("theta0", theta0);
 
+            RCLCPP_INFO(get_logger(), "stuff: %d, %d, %d, %d", rate, x0, y0, theta0);
+
+            geometry_msgs::msg::TransformStamped t;
+            t.header.stamp = get_clock()->now();
+            t.header.frame_id = "nusim/world";
+            t.child_frame_id = "red/base_footprint";
+            t.transform.translation.x = x0;
+            t.transform.translation.y = y0;
+            t.transform.translation.z = 0.0;
+            tf2::Quaternion q;
+            q.setRPY(0, 0, theta0);
+            t.transform.rotation.x = q.x();
+            t.transform.rotation.y = q.y();
+            t.transform.rotation.z = q.z();
+            t.transform.rotation.w = q.w();
+            // std::unique_ptr<tf2_ros::TransformBroadcaster> tf2_rostf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+            tf2_rostf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
             timestep_pub_ = create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
             timer_ = create_wall_timer(std::chrono::milliseconds(int(1.0/rate*1000)), std::bind(&Nusim::timer_callback, this));
             reset_srv_ = create_service<std_srvs::srv::Empty>(
@@ -56,6 +76,8 @@ class Nusim : public rclcpp::Node
             // RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Publish count " << this->count_);
             // RCLCPP_INFO(get_logger(), "stuff: %ld", timestep);
             timestep_pub_->publish(ts);
+            t.header.stamp = get_clock()->now();
+            tf2_rostf_broadcaster_->sendTransform(t);
             timestep += 1;
         }
         void reset(const std_srvs::srv::Empty::Request::SharedPtr request, 
@@ -68,6 +90,8 @@ class Nusim : public rclcpp::Node
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr timestep_pub_;
         rclcpp::Service<std_srvs::srv::Empty>::SharedPtr reset_srv_;
+        std::unique_ptr<tf2_ros::TransformBroadcaster> tf2_rostf_broadcaster_;
+        geometry_msgs::msg::TransformStamped t;
 };
 
 int main(int argc, char* argv[])
