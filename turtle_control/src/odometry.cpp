b31dd9f57597b94c3d1451ea558a35e6ede53c95
Ayush Gaggar <gaggarayush@gmail.com>
2023-02-04 13:16:36 -0600

    finished task E.2

--- turtle_control/src/odometry.cpp
+++ turtle_control/src/odometry.cpp
@@ -15,6 +15,7 @@
 /// CLIENTS:
 ///     none
 /// http://docs.ros.org/en/noetic/api/nav_msgs/html/msg/Odometry.html
+/// ros2 run turtle_control odometry --ros-args -p body_id:="hi" -p odom_id:="hi" -p wheel_left:="left" -p wheel_right:="right"
 
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
@@ -23,6 +24,7 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include "geometry_msgs/msg/quaternion.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "turtle_control/srv/teleport.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
@@ -76,14 +78,26 @@ private:
   rclcpp::TimerBase::SharedPtr timer;
   turtlelib::DiffDrive nubot;
   sensor_msgs::msg::JointState js_msg;
-
   std::vector<double> config{0.0, 0.0, 0.0}; // x, y, theta
+  geometry_msgs::msg::TransformStamped t;
 
   /// \brief Timer callback
   void timer_callback()
   {
     // current_time = get_clock()->now();
-    RCLCPP_INFO(get_logger(), "node works");
+    // RCLCPP_INFO(get_logger(), "node works");
+    t.header.stamp = get_clock()->now();
+    t.header.frame_id = odom_id;
+    t.child_frame_id = body_id;
+    t.transform.translation.x = nubot.getCurrentConfig().at(0);
+    t.transform.translation.y = nubot.getCurrentConfig().at(1);
+    t.transform.translation.z = 0.0;
+    tf2::Quaternion q;
+    q.setRPY(0, 0, nubot.getCurrentConfig().at(2));
+    q.normalize();
+    geometry_msgs::msg::Quaternion q_geom = tf2::toMsg(q);
+    t.transform.rotation = q_geom;
+    tf2_rostf_broadcaster->sendTransform(t);
   }
 
   void js_callback(const sensor_msgs::msg::JointState js) {
@@ -95,9 +109,9 @@ private:
                        turtle_control::srv::Teleport::Response::SharedPtr response) {
     RCLCPP_INFO(get_logger(), "Teleport service...");
     nubot.setCurrentConfig(std::vector<double>{request->x, request->y, request->theta});
-    // config.at(0) = request->x;
-    // config.at(1) = request->y;
-    // config.at(2) = request->theta;
+    config.at(0) = request->x;
+    config.at(1) = request->y;
+    config.at(2) = request->theta;
   }
 
   nav_msgs::msg::Odometry compute_odom() {
