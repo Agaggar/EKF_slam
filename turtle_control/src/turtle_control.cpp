85b4229e460022c266f6a759db6e5f544e012fef
Ayush Gaggar <gaggarayush@gmail.com>
2023-02-05 23:46:52 -0600

    fixing wheel_cmd function

--- turtle_control/src/turtle_control.cpp
+++ turtle_control/src/turtle_control.cpp
@@ -20,6 +20,7 @@
 ///     none
 
 #include <cstdint>
+#include <math.hpp>
 #include "rclcpp/rclcpp.hpp"
 #include "rcl_interfaces/msg/parameter_descriptor.hpp"
 #include "geometry_msgs/msg/twist.hpp"
@@ -124,8 +125,8 @@ private:
 
   turtle_control::msg::WheelCommands conv_vel_to_tick(std::vector<double> wheel_vel) {
     auto cmd = turtle_control::msg::WheelCommands();
-    cmd.left_velocity = int32_t (wheel_vel.at(0)/motor_cmd_per_rad_sec);
-    cmd.right_velocity = int32_t (wheel_vel.at(0)/motor_cmd_per_rad_sec);
+    cmd.left_velocity = int32_t fmod(wheel_vel.at(0)/motor_cmd_per_rad_sec, motor_cmd_max);
+    cmd.right_velocity = int32_t fmod(wheel_vel.at(1)/motor_cmd_per_rad_sec, motor_cmd_max);
     return cmd;
   }
 
