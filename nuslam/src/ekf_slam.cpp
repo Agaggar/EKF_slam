224aa7cfabb343763d8ae61f3f2b1b819a50edd2
Agaggar <gaggarayush@gmail.com>
2023-02-25 22:33:25 -0600

    setting up callback functions for ekf

--- nuslam/src/ekf_slam.cpp
+++ nuslam/src/ekf_slam.cpp
@@ -35,6 +35,7 @@
 #include "nuturtlebot_msgs/msg/sensor_data.hpp"
 #include "turtlelib/diff_drive.hpp"
 #include "builtin_interfaces/msg/time.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
 
@@ -43,9 +44,33 @@ class Ekf_slam : public rclcpp::Node
   public:
     Ekf_slam():
       Node("slam"),
-      rate(5),
-      state(std::vector<double>{0.0,0.0, 0.0})
+      rate(5.0),
+      timestep(0),
+      state_curr(std::vector<double>{0.0,0.0, 0.0}),
+      state_next(std::vector<double>{0.0,0.0, 0.0}),
+      wheel_radius(0.033),
+      track_width(0.16), 
+      motor_cmd_per_rad_sec(1.0 / 0.024), 
+      encoder_ticks_per_rad(651.8986)
     {
+      declare_parameter("wheel_radius", rclcpp::ParameterValue(-1.0));
+      declare_parameter("track_width", rclcpp::ParameterValue(-1.0));
+      declare_parameter("motor_cmd_per_rad_sec", rclcpp::ParameterValue(-1.0));
+      declare_parameter("encoder_ticks_per_rad", rclcpp::ParameterValue(-1.0));
+      get_parameter("wheel_radius", wheel_radius);
+      get_parameter("track_width", track_width);
+      get_parameter("motor_cmd_per_rad_sec", motor_cmd_per_rad_sec);
+      get_parameter("encoder_ticks_per_rad", encoder_ticks_per_rad);
+      std::vector<double> params_set {wheel_radius, track_width,
+                                      motor_cmd_per_rad_sec, encoder_ticks_per_rad};
+
+      for (unsigned int i = 0; i < params_set.size(); i++) {
+        if (params_set.at(i) == -1.0) {
+          RCLCPP_ERROR(get_logger(), "One or more parameters not set!");
+          rclcpp::shutdown();
+        }
+      }
+
       wheel_cmd_sub = create_subscription<nuturtlebot_msgs::msg::WheelCommands>(
       "~/wheel_cmd", 10, std::bind(
         &Ekf_slam::wheel_cmd_callback, this,
@@ -62,10 +87,15 @@ class Ekf_slam : public rclcpp::Node
 
   private:
     double rate;
-    std::vector<double> state;
+    size_t timestep;
+    std::vector<double> state_curr, state_next;
+    double wheel_radius, track_width, motor_cmd_per_rad_sec, encoder_ticks_per_rad;
     rclcpp::Subscription<nuturtlebot_msgs::msg::WheelCommands>::SharedPtr wheel_cmd_sub;
     rclcpp::Subscription<nuturtlebot_msgs::msg::SensorData>::SharedPtr sd_sub;
     rclcpp::TimerBase::SharedPtr timer;
+    
+    turtlelib::Twist2D u{0.0, 0.0, 0.0};
+
 
     /// \brief main timer callback
     void timer_callback() {
@@ -73,7 +103,8 @@ class Ekf_slam : public rclcpp::Node
     }
 
     /// \brief Wheel_cmd subscription
-    void wheel_cmd_callback(nuturtlebot_msgs::msg::WheelCommands cmd)
+    /// \param cmd - wheel command message
+    void wheel_cmd_callback(nuturtlebot_msgs::msg::WheelCommands)
     {
       // std::vector<double> wheel_velocities{0.0, 0.0};
       // wheel_velocities.at(0) = cmd.left_velocity / motor_cmd_per_rad_sec; // * max_rot_vel / 265.0;
@@ -85,8 +116,17 @@ class Ekf_slam : public rclcpp::Node
     }
 
     /// \brief sensor data callback
-    void sd_callback(nuturtlebot_msgs::msg::SensorData) {
-      ; // do stuff
+    /// \param sd - sensor data input
+    void sd_callback(nuturtlebot_msgs::msg::SensorData sd) {
+      encoder_to_rad(sd.left_encoder, sd.right_encoder);
+    }
+
+    /// \brief helper function to convert encoder data to radians  
+    /// \param left_encoder - encoder ticks, left wheel  
+    /// \param right_encoder - encoder ticks, right wheel
+    /// \return radian values for each wheel 
+    std::vector<double> encoder_to_rad(int32_t left_encoder, int32_t right_encoder) {
+      return std::vector<double> {double(left_encoder)/encoder_ticks_per_rad, double(right_encoder)/encoder_ticks_per_rad};
     }
 };
 
