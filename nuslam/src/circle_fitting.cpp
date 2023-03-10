0fc0f2ecdac9aff665ba599d7a331226d60ad2f9
Agaggar <gaggarayush@gmail.com>
2023-03-10 13:13:14 -0600

    implementing circle fitting algorithm

--- nuslam/src/circle_fitting.cpp
+++ nuslam/src/circle_fitting.cpp
@@ -1,5 +1,5 @@
 /// \file
-/// \brief circle fitting algorithm
+/// \brief circle fitting algorithm (from https://nu-msr.github.io/navigation_site/lectures/circle_fit.html)
 ///
 /// PARAMETERS:
 ///     rate (double): frequency of timer callback (hertz), defaults to 200
@@ -44,10 +44,10 @@ class CircleFit : public rclcpp::Node {
         double rate;
         rclcpp::Subscription<visualization_msgs::msg::MarkerArray>::SharedPtr cluster_sub;
         rclcpp::TimerBase::SharedPtr timer;
-        mat clusters;
-        vec x_coor;
-        vec y_coor;
+        mat clusters, data_points, bigZ, bigM, bigH, bigH_inv;
+        arma::vec x_coor, y_coor, zi, bigA;
         std::vector<double> means;
+        double zbar;
 
         /// \brief Timer callback 
         void timer_callback() {
@@ -57,21 +57,30 @@ class CircleFit : public rclcpp::Node {
         /// \brief subscription callback for clusters
         /// \param clus - marker array from topic
         void cluster_callback(visualization_msgs::msg::MarkerArray clus) {
-            // received_clusters.resize(clus.markers.size());
             for (size_t loop = 0; loop < clus.markers.size(); loop++) {
-                // received_clusters.at(loop) = {};
                 if (clus.markers.at(loop).action == 0) {
                     x_coor.zeros(clus.markers.at(loop).points.size());
                     y_coor.zeros(clus.markers.at(loop).points.size());
                     for (size_t index = 0; index < clus.markers.at(loop).points.size(); index++) {
                         x_coor(index) = clus.markers.at(loop).points.at(index).x;
                         y_coor(index) = clus.markers.at(loop).points.at(index).x;
-                        // received_clusters.at(loop).push_back(clus.markers.at(loop).points.at(index).x);
-                        // received_clusters.at(loop).push_back(clus.markers.at(loop).points.at(index).y);
                     }
                     means = computeMean(x_coor, y_coor);
-                    RCLCPP_ERROR_STREAM(get_logger(), "x: , y: \n" << arma::join_rows(x_coor, y_coor));
-                    RCLCPP_INFO(get_logger(), "xMean: %.4f, yMean: %.4f", means.at(0), means.at(1));
+                    // RCLCPP_INFO(get_logger(), "xMean: %.4f, yMean: %.4f", means.at(0), means.at(1));
+                    data_points = arma::join_rows(x_coor, y_coor);
+                    x_coor = shiftPoints(data_points, means).col(0);
+                    y_coor = shiftPoints(data_points, means).col(1);
+                    zi = distToCentroid(shiftPoints(data_points, means));
+                    zbar = findMean(zi);
+                    bigZ = formDataMat(zi, x_coor, y_coor);
+                    RCLCPP_ERROR_STREAM(get_logger(), "capital Z: \n" << bigZ);
+                    bigM = formMoment(bigZ);
+                    RCLCPP_ERROR_STREAM(get_logger(), "bigM: \n" << bigM);
+                    bigH = formConstraint(zbar);
+                    RCLCPP_ERROR_STREAM(get_logger(), "bigH: \n" << bigH);
+                    bigH_inv = bigH.i();
+                    bigA = computeA(bigZ, bigH);
+                    // RCLCPP_INFO(get_logger(), "xMean: %.4f, yMean: %.4f", means.at(0), means.at(1));
                 }
             }
         }
@@ -80,14 +89,14 @@ class CircleFit : public rclcpp::Node {
         /// @param x - x coordinates
         /// @param y - y coordinaes
         /// @return std::vector<double> with elements being the mean of each vec
-        std::vector<double> computeMean(arma::vec x, arma::vec y) {
+        std::vector<double> computeMean(vec x, vec y) {
             return {findMean(x), findMean(y)};
         }
 
         /// \brief helper function to find mean of a column vector 
         /// \param zi - column vec to find mean of 
         /// \return mean 
-        double findMean(arma::vec zi) {
+        double findMean(vec zi) {
             double sum_z = 0;
             for (size_t n = 0; n < zi.n_elem; n++) {
                 sum_z += zi.at(n);
@@ -96,33 +105,47 @@ class CircleFit : public rclcpp::Node {
         }
 
         /// @brief function to shift points to origin
-        /// @param data_points 
-        /// @param centroid 
-        /// @return 
-        arma::mat shiftPoints(arma::mat data_points, std::vector<double> centroid) {
-            return arma::join_cols((data_points.col(0) - centroid.at(0)), (data_points.col(1) - centroid.at(1)));
+        /// @param data_points - matrix of all x and y coor
+        /// @param centroid - center of circle (from ComputeMean)
+        /// @return matrix of x and y coordinates shifted
+        mat shiftPoints(mat data_points, std::vector<double> centroid) {
+            return arma::join_rows((data_points.col(0) - centroid.at(0)), (data_points.col(1) - centroid.at(1)));
         }
 
-        arma::vec distToCentroid(arma::mat data_shifted) {
-            arma::vec xi = data_shifted.col(0);
-            arma::vec yi = data_shifted.col(1);
-            arma::vec zi = xi; // temporarily assign values to zn
+        /// @brief find zi, the distance of each point from the center
+        /// @param data_shifted - the shifted matrix of all x and y points
+        /// @return column vector of distances
+        vec distToCentroid(mat data_shifted) {
+            vec xi = data_shifted.col(0);
+            vec yi = data_shifted.col(1);
+            vec zi = xi; // temporarily assign values to zn
             for (size_t n = 0; n < xi.n_elem; n++) {
                 zi.at(n) = (xi.at(n)*xi.at(n) + yi.at(n)*yi.at(n));
             }
             return zi;
         }
 
-        arma::mat formDataMat(arma::vec zi, arma::vec xi, arma::vec yi) {
-            return arma::join_cols(zi, xi, yi, arma::vec(xi.n_elem, arma::fill::ones));
+        /// @brief form data matrix from data points
+        /// @param zi - all distances
+        /// @param xi - all x coordinates
+        /// @param yi - all y coordinates
+        /// @return matrix of all Z
+        mat formDataMat(vec zi, vec xi, vec yi) {
+            return arma::join_rows(zi, xi, yi, vec(xi.n_elem, arma::fill::ones));
         }
 
-        arma::mat formMoment(arma::mat Z) {
-            return (1.0 / Z.n_rows) * (Z.t()) * Z;
+        /// @brief form moment matrix
+        /// @param Z - data matrix
+        /// @return moment matrix
+        mat formMoment(mat Z) {
+            return (Z.t()) * Z * (1.0 / Z.n_rows);
         }
-
-        arma::mat formConstraint(double z_bar) {
-            arma::mat H = arma::mat(4, 4, arma::fill::eye);
+        
+        /// @brief form constrint matrix for the "Hyperaccurate algebraic fit"
+        /// @param z_bar - average distances
+        /// @return 4x4 constraint matrix
+        mat formConstraint(double z_bar) {
+            mat H = mat(4, 4, arma::fill::eye);
             H.at(0, 0) = 8*z_bar;
             H.at(0, 3) = 2;
             H.at(3, 0) = 2;
@@ -130,14 +153,14 @@ class CircleFit : public rclcpp::Node {
             return H;
         }
 
-        arma::mat computeInv(arma::mat H) {
-            return arma::inv(H);
-        }
-
-        arma::vec computeA(arma::mat Z, arma::mat H) {
+        arma::vec computeA(mat Z, mat H) {
             arma::vec S;
-            arma::mat U, V;
-            arma::svd(U, S, V, Z);
+            mat U, V;
+            arma::svd(U, S, V, Z, "std");
+            // is the minimum value always the bottom right of the diagmat(S)?
+            // U is 3x3
+            // S is 3x3
+            // V is 4x4
             double min_S = 1e12;
             for (size_t n = 0; n < S.n_elem; n++) {
                 if (S.at(n) < min_S) {
@@ -148,8 +171,9 @@ class CircleFit : public rclcpp::Node {
                 return V.col(3);
             }
             else {
-                arma::mat Y = V * arma::diagmat(S) * (V.t());
-                arma::mat Q = Y * (computeInv(H)) * Y;
+                RCLCPP_ERROR_STREAM(get_logger(), "SVD: \n" << U << "\n" << S << "\n" << V);
+                mat Y = V * arma::diagmat(S) * (V.t()); // results in a 4 x 3   
+                mat Q = Y * (H.i()) * Y;
                 arma::cx_vec eigval;
                 arma::cx_mat eigvec;
                 arma::eig_gen(eigval, eigvec, Q);
@@ -161,6 +185,8 @@ class CircleFit : public rclcpp::Node {
                         index = n;
                     }
                 }
+                RCLCPP_ERROR_STREAM(get_logger(), "smalles positive eigenvalue vector: \n" << eigvec(index));
+                // arma::cx_ Astar = eigvec(index);
                 // find eigenvector associated with said eigenvalue (A*), then compute A 
                 // return eigvec.at(index, index) * (arma::inv(Y))
                 // temp return function
